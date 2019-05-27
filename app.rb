d81d68ddf0989d1f39ee7e986cf946ce7ee43284
Mickae Truman <mtruman@nyit.edu>
2019-05-27 05:20:21 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -21,6 +21,7 @@ get '/say/:word1/:word2/:word3/:word4/:word5' do
 end
 
 get '/:operation/:number1/:number2' do
+  @number_1
 case params[:operation]
       when "add" then
         :number1 + :number2
