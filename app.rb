d6f5a40f1486e31a16496e849495e75c7a96d94a
Mickae Truman <mtruman@nyit.edu>
2019-05-27 05:21:11 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -21,7 +21,8 @@ get '/say/:word1/:word2/:word3/:word4/:word5' do
 end
 
 get '/:operation/:number1/:number2' do
-  @number_1
+  @number1 = params[:number1].to_i 
+  @number2 = params[:number2].to_i
 case params[:operation]
       when "add" then
         :number1 + :number2
